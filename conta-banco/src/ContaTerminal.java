961e386786b69a86242c8c41d745e8a9441ad83d
Maicon de Farias <maicondefarias@gmail.com>
2023-08-15 21:28:05 +0100

    Bank Account

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * @auto Maicon Farias
+ * @version 1.0
+ * @since 15/08/2023
+*/
+
 public class ContaTerminal {
+    
+   
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("Digite seu número de usuário:");
+        int numeroUsuario = entrada.nextInt();
+
+        System.out.println("Digite seu número de agência:");
+        String agenciaCliente = entrada.nextLine();
+
+        System.out.println("Digite seu nome completo:");
+        String nomeCliente = entrada.nextLine();
+
+        System.out.println("Digite o seu saldo:");
+        double saldoConta = entrada.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agenciaCliente + ", conta " + numeroUsuario + " e seu saldo " + saldoConta + "€ já está disponnível para saque.");
+
+        
     }
 }
